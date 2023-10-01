6719adffdbf56f3425ad6e4bc778ed3f95be39c2
guga2199 <62083714+guga2199@users.noreply.github.com>
2023-10-01 15:52:12 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const botoes = document.querySelectorAll('.botao');
+
+const personagens = document.querySelectorAll(".personagem");
+
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        desselecionarBotao();
+        botao.classList.add("selecionado");
+
+        desselecionarPesonagem();
+        personagens[indice].classList.add("selecionado");
+    });
+})
+function desselecionarPesonagem() {
+    const personagemSelecinado = document.querySelector(".personagem.selecionado");
+    personagemSelecinado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
